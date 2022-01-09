534089021eb0f37971411b867e8ca29e00190f1b
058058 <82024537+058058@users.noreply.github.com>
2022-01-09 12:30:43 +0800

    Add files via upload

---
+++ php-110-1/user_add_form.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>新增使用者</title></head>
+    <body>
+        <form action=user_add.php method=post>
+            帳號:<input type=text name=id><br>
+            密碼:<input type=text name=pwd><p></p>
+            <input type=submit value=新增> <input type=submit value=清除>
+        </form>
+    </body>
+</html>
